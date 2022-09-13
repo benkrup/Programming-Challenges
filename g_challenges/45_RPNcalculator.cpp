bd7a3ee196ada0fa619c92c41f6497d8ae13bb7c
Ben Krupka <53954301+benkrup@users.noreply.github.com>
2022-09-13 00:19:37 -0400

    Simple implementation of RPN calculator

--- g_challenges/45_RPNcalculator.cpp
+++ g_challenges/45_RPNcalculator.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 int main() {
-    return 0;
+    stack<int> operands;
+    int choice;
+    int num;
+    string op;
+
+    while (true) {
+        cout << "Operand(1), operator(2), or exit(-1)? ";
+        cin >> choice;
+
+        if (choice == 1) {
+            cout << "Input operand: ";
+            cin >> num;
+            operands.push(num);
+        }
+        else if (choice == 2) {
+            cout << "Input operator: ";
+            cin >> op;
+
+            if (operands.size() < 2) {
+                cout << "Too few operands for an operation!" << endl;
+                continue;
+            }
+
+            int val1 = operands.top();
+            operands.pop();
+            int val2 = operands.top();
+            operands.pop();
+
+            if (op == "+") {
+                operands.push(val1 + val2);
+            }
+            else if (op == "-") {
+                operands.push(val1 - val2);
+            }
+            else if (op == "*") {
+                operands.push(val1 * val2);
+            }
+            else if (op == "/") {
+                operands.push(val1 / val2);
+            }
+            else {
+                cout << "Invalid operation!" << endl;
+                operands.push(val2);
+                operands.push(val1);
+            }
+        }
+        else if (choice == -1) {
+            int result = operands.top();
+            cout << result << endl;
+
+            return(0);
+        }
+    }
 }
